6d213f8c5bb96d23d727257d8d9ff92e41bc9dcd
sagrilo <wallace.sagrilo@gmail.com>
2023-10-08 13:01:50 -0300

    API com Dio

---
+++ lib/repositories/home_repository_imp.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:projeto_001/models/post_model.dart';
+import 'package:projeto_001/repositories/home_repository.dart';
+
+class HomeRepositoryImp implements HomeRepository {
+
+  @override
+  Future<List<PostModel>> getList() async {
+    try {
+      var response = await Dio().get('https://jsonplaceholder.typicode.com/posts');
+      return (response.data as List).map((e) => PostModel.fromJson(e)).toList();
+    } catch (e) {
+      print(e);
+    }
+
+    return[];
+  }
+
+
+
+
+}
